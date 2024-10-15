b616f9c4ea103a1450939e9c23b9773d3afa503c
6430301125 <aissariya.t@ku.th>
2024-10-15 11:35:50 +0700

    redesign

--- src/page/Admin.js
+++ src/page/Admin.js
@@ -2,7 +2,7 @@
 import './Admin.css'; // นำเข้าไฟล์ CSS
 function AdminPage() { 
     return (
-        <div className="admin-logs">
+    <div className="admin-logs">
         <div className="header">
           <h1>Admin logs</h1>
         </div>
@@ -10,7 +10,7 @@ function AdminPage() {
           {/* คุณสามารถเพิ่มเนื้อหาของ Logs ที่นี่ */}
           <center>No logs available</center>
         </div>
-        </div>
+    </div>
     );
 }
 
